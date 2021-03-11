c075ee1f882134b030ef58eeb5f5099ae583a2d8
DamianPosada <78286119+damianp91@users.noreply.github.com>
2021-03-10 21:01:03 -0300

    Add files via upload
    recuperatorio

---
+++ RecuIngresoDivF/dos.js
@@ -0,0 +1,84 @@
+function mostrar() {
+    let nombre, carrera, sexo, cantMaerias,
+        notaProm,
+        edad, mejorPf = 0,
+        nomMpf, flag = 1,
+        edadMj, nomMj, flag2 = 1,
+        contF = 0,
+        contQ = 0,
+        contS = 0,
+        porcF = 0,
+        porcQ = 0,
+        porcS = 0,
+        mayMate, nomMaymate, flag3 = 1,
+        edadMaymate;
+    for (i = 0; i < 2; i++) {
+        nombre = prompt("Igrese su nombre: ");
+        carrera = prompt("Ingrese carrera QUIMICA/FISICA/SISTEMAS: ").toUpperCase();
+        while (carrera != "QUIMICA" && carrera != "FISICA" && carrera != "SISTEMAS") {
+            carrera = prompt("Error. Ingrese carrera QUIMICA/FISICA/SISTEMAS: ").toUpperCase();
+        }
+        sexo = prompt("Elija género FEMENINO/MASCULINO/NO BINARIO: ").toUpperCase();
+        while (sexo != "FEMENINO" && sexo != "MASCULINO" && sexo != "NO BINARIO") {
+            sexo = prompt("Error. Elija género FEMENINO/MASCULINO/NO BINARIO: ").toUpperCase();
+        }
+        cantMaerias = parseInt(prompt("Ingrese cantidad de materias: "));
+        while (isNaN(cantMaerias) || cantMaerias < 1 || cantMaerias > 5) {
+            cantMaerias = parseInt(prompt("Error. Ingrese cantidad de materias: "));
+        }
+        notaProm = parseInt(prompt("Ingrese promedio de nota: "));
+        while (isNaN(notaProm) || notaProm < 0 || notaProm > 10) {
+            notaProm = parseInt(prompt("Error. Ingrese promedio de nota entre 0 a 10: "));
+        }
+        edad = parseInt(prompt("Ingrese su edad: "));
+        while (isNaN(edad) || edad < 18) {
+            edad = parseInt(prompt("Error. Ingrese su edad de 18 en adelante: "));
+        }
+        //a
+        if (carrera == "FISICA" && (flag || notaProm > mejorPf)) {
+            mejorPf = notaProm;
+            nomMpf = nombre;
+            flag = 0;
+        }
+        //b
+        if (sexo == "FEMENINO" && (flag2 || edad < edadMj)) {
+            edadMj = edad;
+            nomMj = nombre;
+            flag2 = 0;
+        }
+        //c
+        if (carrera == "FISICA") {
+            contF++;
+            porcF = contF * 100 / 500
+        } else if (carrera == "QUIMICA") {
+            contQ++;
+            porcQ = contQ * 100 / 500
+        } else {
+            contS++;
+            porcS = contS * 100 / 500
+        }
+        //d
+        if (carrera != "QUIMICA" && (flag3 || cantMaerias > mayMate)) {
+            mayMate = cantMaerias;
+            nomMaymate = nombre;
+            edadMaymate = edad;
+            flag3;
+        }
+    } // fin del for
+    if (flag) {
+        console.log(`a) No se registran alumnos de la carrera de Física`);
+    } else {
+        console.log(`a) El nombre del alumno con mejor promedio en FISICA es: ${nomMpf} con un promedio de ${mejorPf}`);
+    }
+    if (flag2) {
+        console.log(`b) No se registran alumnas cursando`);
+    } else {
+        console.log(`b) El nombre de la alumna mas joven es: ${nomMj}, con una edad de ${edadMj} años`);
+    }
+    console.log(`c) el promedio por carrera es: FISICA = ${porcF}%, QUIMICA = ${porcQ}% y SISTEMAS = ${porcS}`);
+    if (flag3) {
+        console.log(`d) No se registra estudiantes distintos a QUIMICA`);
+    } else {
+        console.log(`d) La edad y nombre de la persona que cursa carreras diferentes a QUIMICA es: ${nomMaymate} con ${edadMaymate} años y una cantidad de ${mayMate} materias`);
+    }
+} //fin.
---
+++ RecuIngresoDivF/uno.js
@@ -0,0 +1,82 @@
+function mostrar() {
+    let nombre, edad, sexo, puesto, salario, seguir, sueldoP = 0,
+        sueldoA = 0,
+        sueldoQ = 0,
+        contP = 0,
+        contA = 0,
+        contQ = 0,
+        promP = 0,
+        promA = 0,
+        PromQ = 0,
+        sexoMaysala, mayorSala, flag = 1,
+        nomFem, sueldFem, flag2 = 1,
+        contNob = 0;
+    do {
+        nombre = prompt("Igrese su nombre: ");
+        edad = parseInt(prompt("Ingrese su edad: "));
+        while (isNaN(edad) || edad < 18) {
+            edad = parseInt(prompt("Error. Ingrese su edad de 18 en adelante: "));
+        }
+        sexo = prompt("Elija género FEMENINO/MASCULINO/NO BINARIO: ").toUpperCase();
+        while (sexo != "FEMENINO" && sexo != "MASCULINO" && sexo != "NO BINARIO") {
+            sexo = prompt("Error. Elija género FEMENINO/MASCULINO/NO BINARIO: ").toUpperCase();
+        }
+        puesto = prompt("Igrese el puesto programador/analista/qa: ").toLowerCase();
+        while (puesto != "programador" && puesto != "analista" && puesto != "qa") {
+            puesto = prompt("Error. Igrese el puesto programador/analista/qa: ").toLowerCase();
+        }
+        salario = parseInt(prompt("Ingrese monto de salario de 15000 a 70000: "));
+        while (salario < 15000 || salario > 70000) {
+            salario = parseInt(prompt("Error. Ingrese monto de salario de 15000 a 70000: "));
+        }
+        //a
+        if (puesto == "programador") {
+            sueldoP += salario;
+            contP++;
+        } else if (puesto == "analista") {
+            sueldoA += salario;
+            contA++;
+        } else {
+            sueldoQ += salario;
+            contQ++;
+        }
+        //b
+        if (flag || salario > mayorSala) {
+            mayorSala = salario;
+            sexoMaysala = sexo;
+            flag = 0;
+        }
+        //c
+        if (sexo == "FEMENINO" && (flag2 || salario > sueldFem)) {
+            sueldFem = salario;
+            nomFem = nombre;
+            flag2 = 0;
+        }
+        //d
+        if (sexo == "NO BINARIO" && (salario >= 20000 || salario <= 55000)) {
+            contNob++;
+        }
+        seguir = prompt("¿Desea continuar? s/n: ");
+    } while (seguir == "s");
+    if (contP != 0) {
+        promP = sueldoP / contP;
+    }
+    if (contA != 0) {
+        promA = sueldoA / contA;
+    }
+    if (contQ != 0) {
+        PromQ = sueldoQ / contQ;
+    }
+    console.log(`a) El promedio de los sueldos es: programador ${promP} ARS, analista ${promA} ARS y Qa ${PromQ} ARS respectivamente`);
+    console.log(`b) EL sexo del que percibe el mayor salario es: ${sexoMaysala}, con un sueldo se ${mayorSala} ARS`);
+    if (flag2) {
+        console.log(`c) No se registran empleados femeninos`);
+    } else {
+        console.log(`c) El nombre del elpleado femenino con mayor sueldo es ${nomFem}, con un salario de ${sueldFem}`);
+    }
+    if (contNob == 0) {
+        console.log(`d) No se registran empleados no binarios`);
+    } else {
+        console.log(`d) Hay ${contNob} empleados NO BINARIO con un sueldo entre 20000 a 55000`);
+    }
+} //Fin del código.
